1e22456805a8c34acd20b66f4eae58b8e852f4c1
chiuney <yokota.chiune@gmail.com>
2020-10-07 13:03:38 +0900

    devise gemで新規登録機能とログイン機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "home#index"
+  delete '/users/:id', to: 'users#destroy', as: 'destroy_user_account'
+  resources :users
+  # resource :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
