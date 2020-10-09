9fcccaf29ace40ec6892c8bd24f8a6fb1a3e37a5
chiuney <yokota.chiune@gmail.com>
2020-10-09 13:54:29 +0900

    shopsコントローラーにnew,createアクションを追加

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,4 +1,19 @@
 class ShopsController < ApplicationController
   def new
+    @shop = Shop.new
   end
+
+  def create
+    @shop = Shop.new(shop_params)
+    if @shop.save
+      redirect_to list_path(id: @shop.id)
+    else
+      render new_shop_path
+    end
+  end
+
+  private
+    def shop_params
+      params.permit(:shop_name, :shop_comment)
+    end
 end
