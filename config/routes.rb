64d373539bd71f9a12822b6b260406802b81bde6
chiuney <yokota.chiune@gmail.com>
2020-10-09 12:06:15 +0900

    shopsのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   post "/lists/new", to:"lists#new", as:"new_lists"
   patch "/lists/:id", to:"lists#update", as:"update_lists"
   resources :lists
+  resources :shops
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
