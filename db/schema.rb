b716c36929d0ec4b9f9dcff1e1a356b09b1665b6
chiuney <yokota.chiune@gmail.com>
2020-10-14 14:57:51 +0900

    relationshipモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_052330) do
+ActiveRecord::Schema.define(version: 2020_10_14_055442) do
 
   create_table "lists", force: :cascade do |t|
     t.string "list_name"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2020_10_09_052330) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "shop_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "shops", force: :cascade do |t|
