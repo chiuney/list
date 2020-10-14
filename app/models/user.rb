fbc769b5917d47b84d0fa698d6c295f95a97f58e
chiuney <yokota.chiune@gmail.com>
2020-10-14 14:59:52 +0900

    userとrelationshipのアソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   has_many :lists, dependent: :destroy
   has_many :shops,          dependent: :destroy
+
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy   
+
+  has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
+  has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
