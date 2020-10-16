f8273d96d438af6f161e054a9d93df79979ac856
chiuney <yokota.chiune@gmail.com>
2020-10-16 19:43:35 +0900

    リスト追加、プロフォール編集権限のif文を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
 
   def show
     @user  = User.find(params[:id])
-    @list = List.find(params[:id])
     @lists = List.where(user_id: @user.id)
   end
 
