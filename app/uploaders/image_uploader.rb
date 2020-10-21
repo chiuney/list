db99a94ce5f750d2314af1aef19c51db2a5d5153
chiuney <yokota.chiune@gmail.com>
2020-10-21 22:20:43 +0900

    他のフォロワーのアイコンが表示されないエラーを修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -37,6 +37,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fill: [100, 100]
   end
 
+  version :thumb do
+    process resize_to_fill: [700, 700]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
