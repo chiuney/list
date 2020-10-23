f49afa052969afca076e83e6d70f8257e7d8fb3d
chiuney <yokota.chiune@gmail.com>
2020-10-23 19:20:23 +0900

    マイページのリスト一覧に各リストの登録店舗数を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user  = User.find(params[:id])
-    @lists = List.where(user_id: @user.id)
+    @lists = List.preload(:shops).where(user_id: @user.id)
   end
 
   def destroy
