ceab7a3bb236d0deaa3d67d66a8cb9afaf17a2a4
chiuney <yokota.chiune@gmail.com>
2020-10-25 18:47:11 +0900

    全てのお店にも店舗数を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def show
     @user  = User.find(params[:id])
     @lists = List.preload(:shops).where(user_id: @user.id)
+    @shops = Shop.preload(:shops).where(user_id: @user.id)
   end
 
   def destroy
