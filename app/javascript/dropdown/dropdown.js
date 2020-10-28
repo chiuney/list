9cf358a33e97ce0ebe87f5268330aa5a160c87a4
chiuney <yokota.chiune@gmail.com>
2020-10-28 13:58:51 +0900

    _header_signed_inにドロップダウンメニューを追加

---
+++ app/javascript/dropdown/dropdown.js
@@ -0,0 +1,12 @@
+let dropDown = function () {
+  document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementById('dropdown__btn');
+    if(btn) {
+      btn.addEventListener('click', function(){
+        this.classList.toggle('is-open');
+      });
+    }
+  });
+}();
+
+window.dropDown = dropDown;
