2a251041209b42d2d6b967c1b338ba0178e31a2a
chiuney <yokota.chiune@gmail.com>
2020-10-28 17:50:57 +0900

    user_commentカラムを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :email , :user_comment])
   end
 end
