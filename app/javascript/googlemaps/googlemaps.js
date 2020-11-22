dd21159e9f683b468eed03b41355d29d72a5989a
chiuney <yokota.chiune@gmail.com>
2020-11-22 17:37:53 +0900

    shopの住所をgooglemaps上で表示

--- app/javascript/googlemaps/googlemaps.js
+++ app/javascript/googlemaps/googlemaps.js
@@ -1,81 +1,51 @@
-function initAutocomplete() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: {lat: 35.681382, lng:139.76608399999998},
-    zoom: 15,
-    mapTypeId: "roadmap",
-  });
+//pタグ内の住所が取得できているか確認するため、ウェブコンソールにメッセージ出力
+// console.log(document.getElementById("mapAddress").textContent);
 
-    // ピンの表示
-  marker = new google.maps.Marker({
-    position:  {lat: 35.681382, lng:139.76608399999998},
-    map: map
-  });
+//pタグ内の住所を取得し、変数「my_address」に格納
+var my_address = document.getElementById("mapAddress").textContent;
 
-  // 検索ボックスを作成してUI要素にリンク
-  const input = document.getElementById("pac-input");
-  const searchBox = new google.maps.places.SearchBox(input);
 
-   // SearchBox の結果を現在のマップのビューポートに向けてバイアスかける
-  map.addListener("bounds_changed", () => {
-    searchBox.setBounds(map.getBounds());
-  });
-  let markers = [];
-  // [START maps_places_searchbox_getplaces]
-  // ユーザーが予測を選択したときに発生するイベントをリッスンして
-  // その場所の詳細を表示
-  searchBox.addListener("places_changed", () => {
-    const places = searchBox.getPlaces();
+function initMap() {
 
-    if (places.length == 0) {
-      return;
-    }
-    // 古いマーカーを消去
-    markers.forEach((marker) => {
-      marker.setMap(null);
-    });
-    markers = [];
-    // それぞれの場所について、アイコン、名前、場所を取得
-    const bounds = new google.maps.LatLngBounds();
-    const request = {
-      placeId: "ChIJN1t_tDeuEmsRUsoyG83frY4",
-      fields: ["name", "formatted_address", "place_id", "geometry"],
+    //マップのzoomレベル、タイプの指定
+    var opts = {
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    
-    places.forEach((place) => {
-      if (!place.geometry) {
-        console.log("返された場所にはジオメトリが含まれていません。");
-        return;
+
+   //id「my_map」要素を取得、Mapクラスのインスタンスを作成することで地図を作成
+    var my_google_map = new google.maps.Map(document.getElementById('map'), opts);
+
+    //ジオコードオブジェクト
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode(
+      {
+        //Geocoderにジオコーディングリクエストに送信。
+       //検索する住所文字列と、国コードを指定
+        'address': my_address,
+        'region': 'jp'
+      },
+      function(result, status){
+
+        // ステータスがOKの場合
+        if(status==google.maps.GeocoderStatus.OK){
+
+            //「result[0].geometry.location;」で緯度経度データを取得
+            var latlng = result[0].geometry.location;
+            //指定の座標で中心位置を指定
+            my_google_map.setCenter(latlng);
+            //マーカーを立てる場所の指定
+            var marker = new google.maps.Marker({position:latlng, map:my_google_map, title:latlng.toString(), draggable:true});
+
+            //「マーカー」の「ドラッグ操作が終わった時(dragend)」に関数を実行
+            google.maps.event.addListener(marker, 'dragend', function(event){
+            //マーカーにポインターをホバーした時に、文字列に変換された緯度経度が書かれたツールチップ(吹き出し)を表示
+                marker.setTitle(event.latLng.toString());
+            });
+        }
       }
-
-      const icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25),
-      };
-      // 場所ごとにマーカーを作成
-      markers.push(
-        new google.maps.Marker({
-          map,
-          icon,
-          title: place.name,
-          position: place.geometry.location,
-        })
-      );
-
-      if (place.geometry.viewport) {
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
-  // [END maps_places_searchbox_getplaces]
-
-}
-// [END maps_places_searchbox]
+    );
+  }
 
 
-window.initAutocomplete = initAutocomplete;
+window.initMap = initMap;
