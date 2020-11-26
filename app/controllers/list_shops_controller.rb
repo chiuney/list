96f8ba4f2748ebab78723ead313df7a1b530fbd0
chiuney <yokota.chiune@gmail.com>
2020-11-26 10:55:10 +0900

    purgeする時のundefined method map for nil:NilClassを解消

--- app/controllers/list_shops_controller.rb
+++ app/controllers/list_shops_controller.rb
@@ -26,27 +26,30 @@ class ListShopsController < ApplicationController
   def update
     ActiveRecord::Base.transaction do
       shop = Shop.find(params[:shop_id])
-      # photosの削除メソッド
-      if params[:photos_ids] # (photosが登録済みの場合)
+      # photosの削除メソッド(photosが登録済みの場合)
+      if params[:photos_ids] 
         params[:photos_ids].each do |photos_id|
-          photos = shop.photos.find(photos_id)
-          photos.purge
+          photos = ActiveStorage::Attachment.find(photos_id)
+          # purge_later(非同期削除)(purgeだと処理が遅くなる)
+          photos.purge_later
         end
       end
       # eachでshopをlist毎にupdate
-      params[:list_ids].each do |list_id| 
+      params[:list_ids].each do |list_id|
         list_shop = ListShop.find_by(shop_id: params[:shop_id])
         list_shop.update!(shop_id: params[:shop_id], list_id: list_id)
       end
       # photosの追加登録
-      new_photos = params[:photos].map do |photo|
-        ActiveStorage::Blob.create_after_upload! \
-                  io: photo.open,
-                  filename: photo.original_filename,
-                  content_type: photo.content_type
+      if params[:photos].present?
+        new_photos = params[:photos].map do |photo|
+          ActiveStorage::Blob.create_after_upload! \
+                    io: photo.open,
+                    filename: photo.original_filename,
+                    content_type: photo.content_type
+        end
+        shop.photos.attach(new_photos)
+        shop.update!(shop_comment: params[:shop_comment])
       end
-      shop.photos.attach(new_photos)
-      shop.update!(shop_comment: params[:shop_comment])
     end
       flash[:success] = "変更しました"
       redirect_to shop_path
