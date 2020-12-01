16f7e28707b7bc35e05c47a64c5b4f17d4f9102a
chiuney <yokota.chiune@gmail.com>
2020-12-01 23:19:34 +0900

    googlemapsのinfowindowに正しい店名が表示されるように修正

--- app/javascript/googlemaps/googlemaps.js
+++ app/javascript/googlemaps/googlemaps.js
@@ -5,6 +5,16 @@ window.onload = function() {
   // 住所をオブジェクトで取得 => 配列に変換してmap
   // 参考 : https://blog.sushi.money/entry/2017/04/19/114028
   const addresses = [...document.querySelectorAll("#mapAddress")].map((node) => node.textContent);
+  // // 店名を取得
+  const shopNames = [...document.querySelectorAll("#shop_name_js")].map((node) => node.textContent);
+  // 取得した住所と店名をハッシュ化
+  let data = shopNames.map(function(name, index) {
+    return {
+      name: name,
+      address: addresses[index]
+    }
+  })
+
   const googleMapElement = document.getElementById('map');
   const infowindow = new google.maps.InfoWindow();
 
@@ -12,7 +22,7 @@ window.onload = function() {
   if (addresses.length > 0) {
     window.initMap = mappingPinToGoogleMap(addresses, googleMapElement)
   }
-}
+
 
 function mappingPinToGoogleMap(addresses, googleMapElement) {
   // 初期位置設定
@@ -24,13 +34,10 @@ function mappingPinToGoogleMap(addresses, googleMapElement) {
   // bounds(地図の境界)を定義
   const bounds = new google.maps.LatLngBounds();
 
-  // 店名を取得 => 文字列に変換
-  const shopName = [...document.querySelectorAll("#shop_name_js")].map((node) => node.textContent).toString();
-
-  addresses.forEach((address) => {
+  data.forEach((data) => {
     //ジオコードオブジェクト
     const geocoder = new google.maps.Geocoder();
-    geocoder.geocode({ 'address': address, 'region': 'jp' }, (result, status) => {
+    geocoder.geocode({ 'address': data.address, 'region': 'jp' }, (result, status) => {
       // ステータスがOKの場合
       if(status == google.maps.GeocoderStatus.OK) {
 
@@ -43,18 +50,18 @@ function mappingPinToGoogleMap(addresses, googleMapElement) {
         const marker = new google.maps.Marker({
           position: new google.maps.LatLng(latlng),
           map: googleMap,
-        });
+        });   
 
         // マーカーの数によって地図の表示を変更
         // マーカーが複数の場合
-        if (addresses.length > 1) {
+        if (data.length > 1) {
           // 地図表示領域をマーカー位置に合わせて拡大
           bounds.extend(marker.position);
           // 引数に指定した領域を地図に収める
           googleMap.fitBounds(bounds,20);
           // 吹き出し(InfoWindow)の設定
           const infowindow = new google.maps.InfoWindow({
-            content: shopName,
+            content: data.name,
           });
           // マーカーをクリックすると吹き出しを表示
           marker.addListener("click", () => {
@@ -67,4 +74,5 @@ function mappingPinToGoogleMap(addresses, googleMapElement) {
       }
     })
   })
+}
 }
