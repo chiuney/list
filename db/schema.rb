f4bfef8fa232a2538ef6d4e5eaf50a98d6fa3486
chiuney <yokota.chiune@gmail.com>
2020-12-03 17:18:09 +0900

    [RSpec]User登録 => 有効 のテストを記述、正常終了を確認

--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 2020_11_26_024618) do
     t.integer "shop_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "shop_comment"
     t.text "list_shop_comment"
     t.index ["list_id"], name: "index_list_shops_on_list_id"
     t.index ["shop_id"], name: "index_list_shops_on_shop_id"
@@ -86,7 +85,7 @@ ActiveRecord::Schema.define(version: 2020_11_26_024618) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,47 +7,47 @@ RSpec.describe User, type: :model do
     user = User.new(
       user_name: "tarou",
       email: "tarou@example.com",
-      encrypted_password: "password",
-
+      password: "password",
+      password_confirmation: "password",
     )
     expect(user).to be_valid
   end
 
-  it "ユーザー名がない場合 => 無効" do
-  user = User.new(
-    user_name: nil,
-    email: "testman@example.com",
-    encrypted_password: "password",
-   )
-    user.valid? # エラー検証
-    expect(user.errors[:user_name]).to include("can't be blank")
-  end
+  # it "ユーザー名がない場合 => 無効" do
+  # user = User.new(
+  #   user_name: nil,
+  #   email: "tarou@example.com",
+  #   encrypted_password: "password",
+  #  )
+  #   user.valid? # エラー検証
+  #   expect(user.errors[:user_name]).to include("can't be blank")
+  # end
 
-  it "メアドがない場合 => 無効" do
-    user = User.new(
-      user_name: "tarou",
-      email: nil,
-      encrypted_password: "password",
-     )
-      user.valid? # エラー検証
-      expect(user.errors[:email]).to include("can't be blank")
-    end
-  end
+  # it "メアドがない場合 => 無効" do
+  #   user = User.new(
+  #     user_name: "tarou",
+  #     email: nil,
+  #     encrypted_password: "password",
+  #    )
+  #     user.valid? # エラー検証
+  #     expect(user.errors[:email]).to include("can't be blank")
+  #   end
+  # end
 
-  it "パスワードがない場合 => 無効" do
-    user = User.new(
-      user_name: "tarou",
-      email: "testman@example.com",
-      encrypted_password: nil,
-     )
-      user.valid? # エラー検証
-      expect(user.errors[:encrypted_password]).to include("can't be blank")
-    end
-  end
+  # it "パスワードがない場合 => 無効" do
+  #   user = User.new(
+  #     user_name: "tarou",
+  #     email: "tarou@example.com",
+  #     encrypted_password: nil,
+  #    )
+  #     user.valid? # エラー検証
+  #     expect(user.errors[:encrypted_password]).to include("can't be blank")
+  #   end
+  # end
 
-  it "確認用パスワードがない場合 => 無効" do
-  end
+  # it "確認用パスワードがない場合 => 無効" do
+  # end
 
-  it "パスワードと確認用パスワードが一致しない場合 => 無効" do
-  end
+  # it "パスワードと確認用パスワードが一致しない場合 => 無効" do
+  # end
 end
