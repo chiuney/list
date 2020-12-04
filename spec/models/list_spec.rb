7c6b76c583b16c8c8b72c829a41b991eb8c78db5
chiuney <yokota.chiune@gmail.com>
2020-12-04 21:59:54 +0900

    [rspec]userモデルのテストにfactory_botを適用

---
+++ spec/models/list_spec.rb
@@ -0,0 +1,77 @@
+require 'rails_helper'
+
+RSpec.describe List, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(
+      user_name: "tarou",
+      email: "tarou@example.com",
+      password: "password",
+      password_confirmation: "password",
+    )
+  end
+
+  it "リストネームとuser_idがある場合 => 有効" do
+    list = List.new(
+      list_name: "list_name",
+      user_id: "1",
+    )
+    expect(list).to be_valid
+  end
+
+  it "リストネームがない場合 => 有効" do
+    list = List.new(
+      list_name: nil,
+      user_id: "1",
+    )
+    list.valid?
+    expect(list.errors.of_kind?(:list_name, :blank)).to be_truthy
+  end
+
+  # Factory_bot
+  # it "user_idがない場合 => 有効" do
+  #   list = List.new(
+  #     list_name: "list_name",
+  #     user_id: nil,
+  #   )
+  #   list.valid?
+  #   expect(list.errors.of_kind?(:user_id, :blank)).to be_truthy
+  # end
+
+  it "リストネームが50字の場合 => 有効" do
+    list = List.new(
+      list_name: "a" * 50,
+      user_id: "1",
+    )
+    expect(list).to be_valid
+  end
+
+  it "リストネームが51字の場合 => 無効" do
+    list = List.new(
+      list_name: "a" * 51,
+      user_id: "1",
+    )
+    list.valid?
+    expect(list.errors.of_kind?(:list_name, :too_long)).to be_truthy
+  end
+
+  it "リストコメントが500字の場合 => 有効" do
+    list = List.new(
+      list_name: "list_name",
+      list_comment: "a" * 500,
+      user_id: "1",
+    )
+    expect(list).to be_valid
+  end
+
+  it "リストコメントが501字の場合 => 無効" do
+    list = List.new(
+      list_name: "list_name",
+      list_comment: "a" * 501,
+      user_id: "1",
+    )
+    list.valid?
+    expect(list.errors.of_kind?(:list_comment, :too_long)).to be_truthy
+  end
+
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,57 +4,30 @@ RSpec.describe User, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
 
   it "ユーザー名、メアド、パスワード、確認用パスワードがある場合 => 有効" do
-    user = User.new(
-      user_name: "tarou",
-      email: "tarou@example.com",
-      password: "password",
-      password_confirmation: "password",
-    )
-    expect(user).to be_valid
-    # expect(build(:user)).to be_valid
+    expect(build(:user)).to be_valid
   end
 
   it "ユーザー名がない場合 => 無効" do
-    user = User.new(
-      user_name: nil,
-      email: "tarou@example.com",
-      password: "password",
-      password_confirmation: "password",
-    )
-    user.valid? # エラー検証
+    user = build(:user, user_name:nil)
+    user.valid?
     expect(user.errors.of_kind?(:user_name, :blank)).to be_truthy
   end
 
   it "メアドがない場合 => 無効" do
-    user = User.new(
-      user_name: "tarou",
-      email: nil,
-      password: "password",
-      password_confirmation: "password",
-    )
-    user.valid? # エラー検証
+    user = build(:user, email:nil)
+    user.valid?
     expect(user.errors.of_kind?(:email, :blank)).to be_truthy
   end
 
   it "パスワードがない場合 => 無効" do
-    user = User.new(
-      user_name: "tarou",
-      email: "tarou@example.com",
-      password: nil,
-      password_confirmation: "password",
-    )
-    user.valid? # エラー検証
+    user = build(:user, password:nil)
+    user.valid?
     expect(user.errors.of_kind?(:password, :blank)).to be_truthy
   end
 
   it "確認用パスワードがない場合 => 無効" do
-    user = User.new(
-      user_name: "tarou",
-      email: "tarou@example.com",
-      password: "password",
-      password_confirmation: nil,
-    )
-    user.valid? # エラー検証
+    user = build(:user, password_confirmation:nil)
+    user.valid?
     expect(user.errors.of_kind?(:password_confirmation, :blank)).to be_truthy
   end
 
@@ -62,65 +35,35 @@ RSpec.describe User, type: :model do
   # end
 
   it "ユーザーネームが20字の場合 => 有効" do
-    user = User.new(
-      user_name: "a" * 20,
-      email: "tarou@example.com",
-      password: "password",
-      password_confirmation: "password",
-    )
+    user = build(:user, user_name: "a" * 20)
     expect(user).to be_valid
   end
 
   it "ユーザーネームが21字の場合 => 無効" do
-    user = User.new(
-      user_name: "a" * 21,
-      email: "tarou@example.com",
-      password: "password",
-      password_confirmation: "password",
-    )
-      user.valid? # エラー検証
-      expect(user.errors.of_kind?(:user_name, :too_long)).to be_truthy
+    user = build(:user, user_name: "a" * 21)
+    user.valid?
+    expect(user.errors.of_kind?(:user_name, :too_long)).to be_truthy
   end
 
   it "パスワードが5字の場合 => 無効" do
-    user = User.new(
-      user_name: "tarou",
-      email: "tarou@example.com",
-      password: "a" * 5,
-      password_confirmation: "password",
-    )
-    user.valid? # エラー検証
+    user = build(:user, password: "a" * 5)
+    user.valid?
     expect(user.errors.of_kind?(:password, :too_short)).to be_truthy
   end
 
   it "パスワードが6字の場合 => 有効" do
-    user = User.new(
-      user_name: "tarou",
-      email: "tarou@example.com",
-      password: "a" * 6,
-      password_confirmation: "password",
-    )
+    user = build(:user, password: "a" * 6)
     expect(user).to be_valid
   end
 
   it "確認用パスワードが5字の場合 => 無効" do
-    user = User.new(
-      user_name: "tarou",
-      email: "tarou@example.com",
-      password: "password",
-      password_confirmation: "a" * 5,
-    )
-    user.valid? # エラー検証
+    user = build(:user, password_confirmation: "a" * 5)
+    user.valid?
     expect(user.errors.of_kind?(:password_confirmation, :too_short)).to be_truthy
   end
 
   it "確認用パスワードが6字の場合 => 有効" do
-    user = User.new(
-      user_name: "tarou",
-      email: "tarou@example.com",
-      password: "password",
-      password_confirmation: "a" * 6,
-    )
+    user = build(:user, password_confirmation: "a" * 6)
     expect(user).to be_valid
   end
 
