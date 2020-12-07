47f17ee713f848eae51f3386a92aad27ecabb11d
chiuney <yokota.chiune@gmail.com>
2020-12-07 11:23:47 +0900

    [rspec][factory]shopとadd_shopのテスト用データを作成

--- spec/factories/add_shops.rb
+++ spec/factories/add_shops.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :add_shop do
-    
+
+    association :user
+    association :shop
+
   end
 end
--- spec/factories/lists.rb
+++ spec/factories/lists.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     list_name {"list_name"}
 
     association :user
+    association :shop
   end
 end
