d64091b7fc493da334ba8351fbcb3eea8c58f613
chiuney <yokota.chiune@gmail.com>
2020-12-24 23:00:26 +0900

    [rubocop]controllerのリファクタ(途中)

--- app/models/list_shop.rb
+++ app/models/list_shop.rb
@@ -6,4 +6,33 @@ class ListShop < ApplicationRecord
 
   validates :shop_id, uniqueness: { scope: :list_id }, presence: true
   validates :list_id, presence: true
+
+
+  # def upload_photos
+  #     # photosの登録
+  #     # 詳細→https://api.rubyonrails.org/classes/ActiveStorage/Blob.html#method-c-generate_unique_secure_token
+  #     if params[:photos].present? # エラー回避(.map for nil:NilClass)
+  #       new_photos = params[:photos].map do |photo|
+  #         ActiveStorage::Blob.create_and_upload! \
+  #           io: photo.open,
+  #           filename: photo.original_filename,
+  #           content_type: photo.content_type
+  #       end
+  #     end
+
+  #     self.photos.attach(new_photos)
+  # end
+
+  
+
+  # Active Storage バリデーション
+  # validate :content_type
+
+  # private
+
+  # def content_type
+  #   if !photos.content_type.in?(%('photos/jpeg photos/jpg photos/png'))
+  #     errors.add(:photos,'にはjpeg,jpg,pngファイルを使用してください')
+  #   end
+  # end
 end
