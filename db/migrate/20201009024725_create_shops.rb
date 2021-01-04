85691fe9119c913f9af1b01a0c9c7ab7e0dfd6e2
chiuney <yokota.chiune@gmail.com>
2021-01-04 16:38:16 +0900

    test

--- db/migrate/20201009024725_create_shops.rb
+++ db/migrate/20201009024725_create_shops.rb
@@ -4,7 +4,6 @@ class CreateShops < ActiveRecord::Migration[6.0]
   def change
     create_table :shops do |t|
       t.string :shop_name, default: '', null: false
-      t.text :shop_comment
       t.string :ure_id
       t.string :list_id
 
