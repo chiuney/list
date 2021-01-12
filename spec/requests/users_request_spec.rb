7a18594ee9f38a7815177ed96c9e283c6e807f9b
chiuney <yokota.chiune@gmail.com>
2021-01-12 16:36:28 +0900

    [rspec][users_request_spec]テスト追加

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -44,7 +44,7 @@ RSpec.describe 'Users', type: :request do
       it 'valid' do
         user_params = attributes_for(:user)
         post '/users', params: { user: user_params }
-        expect(response).to have_http_status(200)
+        expect(response).to have_http_status(302)
       end
     end
 
@@ -55,34 +55,84 @@ RSpec.describe 'Users', type: :request do
       end
 
       it 'valid' do
-        # user_params = attributes_for(:user, user_name:'jirou')
-        # patch user_registration_path, params: { user: user_params }
-        # expect(response).to have_http_status(200)
+        user_params = attributes_for(:user, user_name:'jirou')
+        patch user_registration_path, params: { user: user_params }
+        expect(response).to have_http_status(302)
+      end
+    end
+
+    context 'GET #destroy' do
+      before do
+        @user = create(:user)
+        sign_in @user
+      end
+
+      it 'valid' do
+        delete '/users'
+        expect(response).to have_http_status(302)
       end
     end
   end
 
   describe 'sessions' do
-    context 'GET #new' do
-      it 'valid' do
+    context 'ユーザーを作成済みの場合' do
+
+      before do
+        @user = create(:user)
       end
 
-      it 'invalid' do
+      context 'GET #new' do
+        it 'valid' do
+          get new_user_session_path
+          expect(response).to have_http_status(200)
+        end
+      end
+
+      context 'GET #create' do
+        it 'valid' do
+          post user_session_path
+          expect(response).to have_http_status(200)
+        end
+      end
+
+      context 'GET #destroy' do
+        it 'valid' do
+          sign_in @user
+          delete destroy_user_session_path
+          expect(response).to have_http_status(302)
+        end
       end
     end
 
-    context 'GET #create' do
-      it 'valid' do
+    context 'ユーザーを未作成・登録情報と違う情報でログインしようとしている場合' do
+      context 'GET #new' do
+        it 'valid' do
+          get new_user_session_path
+          expect(response).to have_http_status(200)
+        end
       end
 
-      it 'invalid' do
+      context 'GET #create' do
+        it 'invalid' do
+          post user_session_path
+          expect(response).to have_http_status(401)
+        end
+      end
+
+      context 'GET #destroy' do
+        let(:user) { create(:user) }
+        before do
+          sign_in user
+        end
+
+        it 'valid' do
+          delete destroy_user_session_path
+          expect(response).to have_http_status(302)
+        end
       end
     end
 
-    context 'GET #destroy' do
-      it 'valid' do
-      end
-    end
+
   end
 end
 
