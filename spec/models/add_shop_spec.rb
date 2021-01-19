7fd06e27dd062d3acb064024c22527df69cfe1a0
chiuney <yokota.chiune@gmail.com>
2021-01-19 22:21:41 +0900

    [rspec]

--- spec/models/add_shop_spec.rb
+++ spec/models/add_shop_spec.rb
@@ -3,19 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe AddShop, type: :model do
-  it 'user_idとshop_idがある場合 => 有効' do
-    expect(create(:add_shop)).to be_valid
-  end
+  describe 'バリデーション' do
+    context 'user_idとshop_idがある場合' do
+      it 'valid' do
+        expect(create(:add_shop)).to be_valid
+      end
+    end
 
-  it 'user_idがない場合 => 無効' do
-    add_shop = build(:add_shop, user_id: nil)
-    add_shop.valid?
-    expect(add_shop.errors.of_kind?(:user_id, :blank)).to be_truthy
-  end
+    context 'user_idがない場合' do
+      it 'invalid' do
+        add_shop = build(:add_shop, user_id: nil)
+        add_shop.valid?
+        expect(add_shop.errors.of_kind?(:user_id, :blank)).to be_truthy
+      end
+    end
 
-  it 'shop_idがない場合 => 無効' do
-    add_shop = build(:add_shop, shop_id: nil)
-    add_shop.valid?
-    expect(add_shop.errors.of_kind?(:user_id, :blank)).to be_truthy
+    context 'shop_idがない場合' do
+      it 'invalid' do
+        add_shop = build(:add_shop, shop_id: nil)
+        add_shop.valid?
+        expect(add_shop.errors.of_kind?(:user_id, :blank)).to be_truthy
+      end
+    end
   end
 end
