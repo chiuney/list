6441d053366895c0ebb1f9b041d7015b7a37dfb2
chiuney <yokota.chiune@gmail.com>
2021-01-22 21:55:04 +0900

    [rspec]

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -3,10 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Homes', type: :request do
-  describe '#index' do
-    it '200レスポンスを返す' do
-      get root_path
-      expect(response).to have_http_status(200)
+
+  describe 'バリデーション' do
+    context '#index' do
+      it 'valid' do
+        get root_path
+        expect(response).to have_http_status(200)
+      end
     end
   end
 end
